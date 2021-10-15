bf1b8fb943223acecfebba55276f573efccbc66f
IgorGutkowski <s24529@pjwstk.edu.pl>
2021-10-15 13:07:06 +0200

    zadanie1-0-0-2

---
+++ 1-0-0-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+auto main() -> int
+{
+	int liczba1,liczba2,wynik;
+	std ::cout<<"Podaj pierwszą liczbę\n";
+	std ::cin >>liczba1;
+	std ::cout<<"Podaj drugą liczbę \n";
+	std ::cin>>liczba2;
+	wynik=liczba1+liczba2;
+	std::cout<<"Suma wynosi " << wynik <<"\n";
+}
+	
+
+

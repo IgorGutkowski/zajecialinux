db7547ffb08ff9d9c106d5c41cdd1a54d2028e2a
IgorGutkowski <s24529@pjwstk.edu.pl>
2021-10-15 13:32:06 +0200

    zadanie 1-0-0-5.cpp

---
+++ 1-0-0-5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+auto main() -> int
+{
+	int liczba;
+	std::cout<<"Podaj liczbę\n";
+	std::cin >>liczba;
+	std::cout<<"wartość bezwględna wynosi: " << abs(liczba) << "\n";
+}
+	

7f8ee3de4c3ec71e4431c19558ab58c9485d2099
IgorGutkowski <91725295+IgorGutkowski@users.noreply.github.com>
2021-10-28 23:04:10 +0200

    zadanie 2.0.0.7

---
+++ 2-0-0-7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+auto main() -> int
+{
+	int a,b,suma=0;
+	std::cout<<"Podaj limit\n";
+	std::cin>>a;
+	std::cout<<"Podaj dzielnik\n";
+	std::cin>>b;
+	
+for(int i=0;i<=a;i++)
+
+	if(i%b==0)
+	suma = suma +i;
+	std::cout<<"Suma liczb podzielnych przez "<<b<<" w tym przedziale wynosi: "<<suma;
+return 0;
+}

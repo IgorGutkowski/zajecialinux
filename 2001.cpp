466a7b26b6b1ef883219c45f8ab66148d68dd911
IgorGutkowski <s24529@pjwstk.edu.pl>
2021-10-29 13:24:43 +0200

    zadanie 2.0.0.1.cpp

---
+++ 2001.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+auto main() -> int
+{
+	int a,b,i;
+	
+std::cout<<"Podaj liczbę: \n";
+std::cin>>a;
+	
+std::cout<<"Podaj drugą liczbę: \n";
+std::cin>>b;
+
+i=a;
+	
+std::cout<<"przedział zawiera liczby:\n";
+for(int i=a;i<b;i++)
+{
+	
+	std::cout<<i<<"\n";
+	
+	
+	
+}
+return 0;
+}
+

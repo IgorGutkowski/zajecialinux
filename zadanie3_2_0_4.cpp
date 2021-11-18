43ffa84661114b80bef33731d4df95f10d1c3866
IgorGutkowski <91725295+IgorGutkowski@users.noreply.github.com>
2021-11-18 20:02:49 +0100

    zadanie 3.2.0.4

---
+++ zadanie3_2_0_4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    auto ve = std::vector<int>();
+    int a;
+    std::cout<<"Podaj iloma liczbami chcesz zapelnic tablice: \n";
+    std::cin>>a;
+
+    for (int i =0; i < a; i++)
+    {
+        int b = rand() % a + 2; // ka¿demu elementowi wektora od 0 do 63 przypisze wartosc losowa od 0 do 64
+        ve.push_back(b);
+        // opcjonalny podglad std::cout<<ve[i]<<std::endl;
+    }
+
+    std::cout<<"Przed sortowaniem"<<std::endl;
+   for (int i= 0; i <a; i++)
+   {
+      std::cout<<ve.at(i)<<std::endl;
+   }
+      std::cout<<"Po sortowaniu"<<std::endl;
+      std::sort(ve.begin(), ve.end());
+   for (int i = (a-1); i >= 0; i--)  // posortowane od najwiekszej do najmniejszej wypisze od konca do poczatku
+   {
+      std::cout<<ve.at(i)<<std::endl;
+   }
+
+return 0;
+}
+

4b69957dd282be380f329c0841965c5f6a818e7c
IgorGutkowski <91725295+IgorGutkowski@users.noreply.github.com>
2021-11-20 19:00:20 +0100

    zadanie 3.2.0.2

---
+++ zadanie3_2_0_2alt.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    int n, rozmiar, j;
+    auto ve = std::vector<int>();
+
+    do
+    {
+        std::cout<<"Podaj liczbe: \n";
+        std::cin>>n;
+        ve.push_back(n);
+    }while(n!=0);
+
+    rozmiar = (ve.size()-1);
+    j = 0;
+    do
+    {
+        std::cout<<ve.at(j);
+        j++;
+        if((j%5==0))
+        {
+            std::cout<<"\n";
+        }
+    }while(j < rozmiar);
+
+return 0;
+}

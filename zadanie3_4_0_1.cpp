2bd25f31a25785e614ef98fd27e7228f78afda24
IgorGutkowski <91725295+IgorGutkowski@users.noreply.github.com>
2021-11-26 12:41:24 +0100

    zadanie 3.4.0.1

---
+++ zadanie3_4_0_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+int main()
+{
+    std::string zmienna = "Igor Gutkowski";
+    std::string * wskaznik;
+    wskaznik =&zmienna;
+
+    std::cout << "wartosc zmiennej = " << *wskaznik << std::endl;
+    std::cout << "adres zmiennej = " <<& *wskaznik << std::endl;
+    std::cout << "rozmiar zmiennej = " << sizeof(*wskaznik) << std::endl;
+    std::cout << "wartosc wskaznika = " << wskaznik << std::endl;
+    std::cout << "adres wskaznika = " <<& wskaznik << std::endl;
+    std::cout << "rozmiar wskaznika = " << sizeof(wskaznik) << std::endl;
+
+    return 0;
+}

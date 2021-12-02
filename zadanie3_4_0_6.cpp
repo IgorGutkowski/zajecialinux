af16d43c30f04382aaf92a7608ab91c2999609fb
IgorGutkowski <91725295+IgorGutkowski@users.noreply.github.com>
2021-12-02 22:13:27 +0100

    zadanie 3.4.0.6

--- zadanie3_4_0_6.cpp
+++ zadanie3_4_0_6.cpp
@@ -11,7 +11,7 @@ void *memset(void *ptr, int x, size_t n);               // ptr ==> Starting addr
     puts (tab);                                         // puts zastepuje petle i cout do wyswietlenia zawartosci stringa
 
 
-    memset(tab + 18,22, 11*sizeof(char));               // (tab +18 - zaczynamy od 18 bajtu tablicy tab) (22 - w ich miejsce wstawiamy te wartosc) (7*sizeof(char) - 7 bajtow zamieniamy)
+    memset(tab + 18,22, 11*sizeof(char));               // (tab +18 - zaczynamy od 19 bajtu tablicy tab czyli tab[18]) (22 - w ich miejsce wstawiamy te wartosc) (7*sizeof(char) - 7 bajtow zamieniamy)
     std::cout<<"Tablica po uzyciu memset: ";
     puts (tab);
 

15de8e872b6a921c31a4ea5b151bb1e60a2342fc
IgorGutkowski <91725295+IgorGutkowski@users.noreply.github.com>
2021-12-02 20:06:26 +0100

    zadanie 3.4.0.6

---
+++ zadanie3_4_0_6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string.h>
+
+void *memset(void *ptr, int x, size_t n);               // ptr ==> Starting address of memory to be filled
+                                                        // x   ==> Value to be filled
+                                                        // n   ==> Number of bytes to be filled starting from ptr to be filled
+  int main()
+  {
+    std::cout<<"Tablica przed uzyciem memset: ";
+    char tab[50] = "Najlepsza woda to Muszynianka";
+    puts (tab);                                         // puts zastepuje petle i cout do wyswietlenia zawartosci tej tablicy
+
+
+    memset(tab + 18,22, 11*sizeof(char));               // (tab +18 - zaczynamy od 18 bajtu) (22 - w ich miejsce wstawiamy te wartosc) (7*sizeof(char) - 7 bajtow zamieniamy)
+    std::cout<<"Tablica po uzyciu memset: ";
+    puts (tab);
+
+return 0;
+  }

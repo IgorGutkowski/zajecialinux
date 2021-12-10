aec6f222a6795086596a781cad0406584f563fce
IgorGutkowski <91725295+IgorGutkowski@users.noreply.github.com>
2021-12-10 11:45:30 +0100

    zadanie 3.5.0.3 memrev

---
+++ zadanie3_5_0_3alt.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <string.h> // strlen
+
+void memrev(void* ta,size_t l)
+{
+    auto proxy = reinterpret_cast<char*>(ta);
+
+    char r[l];
+
+    for(int i = 0; i < l; i++)
+    {
+    r[i] = proxy[l - 1 - i];
+    }
+
+    for(int i = 0; i < l; i++)
+    {
+    proxy[i] = r[i];
+    }
+    std::cout<<proxy;
+}
+
+
+int main()
+{
+    char tab[]="tekst nie posortowany";
+    memrev(tab,strlen(tab));
+    return 0;
+
+}
